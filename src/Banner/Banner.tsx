90d840eeb62ee8c8871c08747b0986baa3ff7f9a
TorqueReborn <torquereborn@gmail.com>
2025-11-09 20:27:45 +0530

    Update banner style

--- src/Banner/Banner.tsx
+++ src/Banner/Banner.tsx
@@ -1,13 +1,16 @@
 import me from '../assets/me.jpg';
 
 const Banner = () => {
-  return (
-    <div className='flex relative'>
-        Full Stack <br />
-        Developer
-        <img src={me} width={200} className='absolute right-0' alt="Photo of Abhinand" />
-    </div>
-  )
+    return (
+        <div className='flex justify-between'>
+            <div className='h-185 overflow-hidden'>
+                <img src={me} width={520} alt="Photo of Abhinand" />
+            </div>
+            <div className='text-8xl'>
+                Abhinand Ammanamkandy
+            </div>
+        </div>
+    )
 }
 
 export default Banner
